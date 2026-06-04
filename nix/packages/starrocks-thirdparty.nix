9feef94f27118ca47dd56a217a2fd85a239f6a9c
Georg Heiler <georg.kf.heiler@gmail.com>
2026-06-04 17:39:18 +0200

    Format Nix package definition

--- nix/packages/starrocks-thirdparty.nix
+++ nix/packages/starrocks-thirdparty.nix
@@ -126,100 +126,100 @@ stdenv.mkDerivation {
   '';
 
   buildPhase = ''
-    runHook preBuild
+        runHook preBuild
 
-    export HOME=$TMPDIR/home
-    mkdir -p "$HOME"
+        export HOME=$TMPDIR/home
+        mkdir -p "$HOME"
 
-    rm -rf thirdparty/src
-    cp -R ${starrocks-thirdparty-sources}/src thirdparty/src
-    chmod -R u+w thirdparty
-    patchShebangs thirdparty/src
-    # Keep Ragel's vendored autotools/parser outputs newer than their inputs.
-    # Otherwise make tries to regenerate them with unavailable automake-1.15.
-    touch thirdparty/src/ragel-6.10/aclocal.m4
-    touch thirdparty/src/ragel-6.10/configure
-    find thirdparty/src/ragel-6.10 -name Makefile.in -exec touch {} +
-    touch \
-      thirdparty/src/ragel-6.10/ragel/rlparse.cpp \
-      thirdparty/src/ragel-6.10/ragel/rlparse.h \
-      thirdparty/src/ragel-6.10/ragel/rlscan.cpp
-    substituteInPlace thirdparty/src/abseil-cpp-20220623.0/absl/container/internal/container_memory.h \
-      --replace-fail '#include "absl/utility/utility.h"' '#include "absl/utility/utility.h"
-    #include <cstdint>'
-    substituteInPlace thirdparty/src/s2geometry-0.9.0/src/s2/third_party/absl/container/internal/container_memory.h \
-      --replace-fail '#include "s2/third_party/absl/utility/utility.h"' '#include "s2/third_party/absl/utility/utility.h"
-    #include <cstdint>'
-    substituteInPlace thirdparty/src/llvm-project-18.1.8.src/llvm/include/llvm/ADT/SmallVector.h \
-      --replace-fail '#include <cstddef>' '#include <cstddef>
-    #include <cstdint>'
-    substituteInPlace thirdparty/src/llvm-project-18.1.8.src/llvm/lib/Target/X86/MCTargetDesc/X86MCTargetDesc.h \
-      --replace-fail '#include <string>' '#include <string>
-    #include <cstdint>'
-    substituteInPlace thirdparty/src/azure-storage-files-shares_12.12.0/sdk/attestation/azure-security-attestation/src/private/crypto/inc/crypto.hpp \
-      --replace-fail '#include <vector>' '#include <vector>
-    #include <cstdint>'
-    substituteInPlace thirdparty/src/arrow-apache-arrow-19.0.1/cpp/cmake_modules/ThirdpartyToolchain.cmake \
-      --replace-fail 'if(CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.0)' \
-        'if(CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.0 AND
-       ARROW_THRIFT_BUILD_VERSION VERSION_LESS 0.23.0)'
-    for header in \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Authentication.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/BrokerConsumerStats.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Client.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ClientConfiguration.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Consumer.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ConsumerConfiguration.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Message.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/MessageBatch.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/MessageBuilder.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ProducerConfiguration.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Reader.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ReaderConfiguration.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/c/consumer_configuration.h \
-      thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/c/reader.h
-    do
-      substituteInPlace "$header" \
-        --replace-fail '#include <pulsar/defines.h>' '#include <pulsar/defines.h>
-    #include <stdint.h>'
-    done
-    substituteInPlace thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/MessageIdBuilder.h \
-      --replace-fail '#include <pulsar/MessageId.h>' '#include <pulsar/MessageId.h>
-    #include <stdint.h>'
-    substituteInPlace thirdparty/build-thirdparty.sh \
-      --replace-fail 'export CXXFLAGS="-O3 -fno-omit-frame-pointer -fPIC -g ' \
-        'export CXXFLAGS="-O3 -fno-omit-frame-pointer -fPIC -g -Wno-error -Wno-array-bounds -Wno-error=array-bounds -Wno-stringop-overflow -Wno-error=stringop-overflow '
-    substituteInPlace thirdparty/build-thirdparty.sh \
-      --replace-fail 'export CFLAGS="-O3 -fno-omit-frame-pointer -fPIC ''${FILE_PREFIX_MAP_OPTION}"' \
-        'export CFLAGS="-O3 -fno-omit-frame-pointer -fPIC -std=gnu17 ''${FILE_PREFIX_MAP_OPTION}"'
-    perl -0pi -e '
-      our $replaced;
-      $replaced += s@(-Dxsimd_DIR=\$TP_INSTALL_DIR/share/cmake/xsimd \.\.\n\n)    \$\{BUILD_SYSTEM\} -j\$PARALLEL\n    \$\{BUILD_SYSTEM\} install@$1    if ! \$BUILD_SYSTEM -j\$PARALLEL; then\n        echo "Arrow parallel build failed; retrying serially for deterministic output"\n        \$BUILD_SYSTEM -j1 VERBOSE=1\n    fi\n    \$BUILD_SYSTEM install@;
-      END { die "failed to patch build_arrow build command\n" unless $replaced }
-    ' thirdparty/build-thirdparty.sh
-    substituteInPlace thirdparty/build-thirdparty.sh \
-      --replace-fail '    ''${BUILD_SYSTEM} -j$PARALLEL
-    ''${BUILD_SYSTEM} install
-}
+        rm -rf thirdparty/src
+        cp -R ${starrocks-thirdparty-sources}/src thirdparty/src
+        chmod -R u+w thirdparty
+        patchShebangs thirdparty/src
+        # Keep Ragel's vendored autotools/parser outputs newer than their inputs.
+        # Otherwise make tries to regenerate them with unavailable automake-1.15.
+        touch thirdparty/src/ragel-6.10/aclocal.m4
+        touch thirdparty/src/ragel-6.10/configure
+        find thirdparty/src/ragel-6.10 -name Makefile.in -exec touch {} +
+        touch \
+          thirdparty/src/ragel-6.10/ragel/rlparse.cpp \
+          thirdparty/src/ragel-6.10/ragel/rlparse.h \
+          thirdparty/src/ragel-6.10/ragel/rlscan.cpp
+        substituteInPlace thirdparty/src/abseil-cpp-20220623.0/absl/container/internal/container_memory.h \
+          --replace-fail '#include "absl/utility/utility.h"' '#include "absl/utility/utility.h"
+        #include <cstdint>'
+        substituteInPlace thirdparty/src/s2geometry-0.9.0/src/s2/third_party/absl/container/internal/container_memory.h \
+          --replace-fail '#include "s2/third_party/absl/utility/utility.h"' '#include "s2/third_party/absl/utility/utility.h"
+        #include <cstdint>'
+        substituteInPlace thirdparty/src/llvm-project-18.1.8.src/llvm/include/llvm/ADT/SmallVector.h \
+          --replace-fail '#include <cstddef>' '#include <cstddef>
+        #include <cstdint>'
+        substituteInPlace thirdparty/src/llvm-project-18.1.8.src/llvm/lib/Target/X86/MCTargetDesc/X86MCTargetDesc.h \
+          --replace-fail '#include <string>' '#include <string>
+        #include <cstdint>'
+        substituteInPlace thirdparty/src/azure-storage-files-shares_12.12.0/sdk/attestation/azure-security-attestation/src/private/crypto/inc/crypto.hpp \
+          --replace-fail '#include <vector>' '#include <vector>
+        #include <cstdint>'
+        substituteInPlace thirdparty/src/arrow-apache-arrow-19.0.1/cpp/cmake_modules/ThirdpartyToolchain.cmake \
+          --replace-fail 'if(CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.0)' \
+            'if(CMAKE_COMPILER_IS_GNUCC AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 15.0 AND
+           ARROW_THRIFT_BUILD_VERSION VERSION_LESS 0.23.0)'
+        for header in \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Authentication.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/BrokerConsumerStats.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Client.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ClientConfiguration.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Consumer.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ConsumerConfiguration.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Message.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/MessageBatch.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/MessageBuilder.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ProducerConfiguration.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/Reader.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/ReaderConfiguration.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/c/consumer_configuration.h \
+          thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/c/reader.h
+        do
+          substituteInPlace "$header" \
+            --replace-fail '#include <pulsar/defines.h>' '#include <pulsar/defines.h>
+        #include <stdint.h>'
+        done
+        substituteInPlace thirdparty/src/pulsar-client-cpp-3.3.0/include/pulsar/MessageIdBuilder.h \
+          --replace-fail '#include <pulsar/MessageId.h>' '#include <pulsar/MessageId.h>
+        #include <stdint.h>'
+        substituteInPlace thirdparty/build-thirdparty.sh \
+          --replace-fail 'export CXXFLAGS="-O3 -fno-omit-frame-pointer -fPIC -g ' \
+            'export CXXFLAGS="-O3 -fno-omit-frame-pointer -fPIC -g -Wno-error -Wno-array-bounds -Wno-error=array-bounds -Wno-stringop-overflow -Wno-error=stringop-overflow '
+        substituteInPlace thirdparty/build-thirdparty.sh \
+          --replace-fail 'export CFLAGS="-O3 -fno-omit-frame-pointer -fPIC ''${FILE_PREFIX_MAP_OPTION}"' \
+            'export CFLAGS="-O3 -fno-omit-frame-pointer -fPIC -std=gnu17 ''${FILE_PREFIX_MAP_OPTION}"'
+        perl -0pi -e '
+          our $replaced;
+          $replaced += s@(-Dxsimd_DIR=\$TP_INSTALL_DIR/share/cmake/xsimd \.\.\n\n)    \$\{BUILD_SYSTEM\} -j\$PARALLEL\n    \$\{BUILD_SYSTEM\} install@$1    if ! \$BUILD_SYSTEM -j\$PARALLEL; then\n        echo "Arrow parallel build failed; retrying serially for deterministic output"\n        \$BUILD_SYSTEM -j1 VERBOSE=1\n    fi\n    \$BUILD_SYSTEM install@;
+          END { die "failed to patch build_arrow build command\n" unless $replaced }
+        ' thirdparty/build-thirdparty.sh
+        substituteInPlace thirdparty/build-thirdparty.sh \
+          --replace-fail '    ''${BUILD_SYSTEM} -j$PARALLEL
+        ''${BUILD_SYSTEM} install
+    }
 
-#mariadb-connector-c' '    ''${BUILD_SYSTEM} -j$PARALLEL
-    rm -rf "$TP_INSTALL_DIR/include/hs"
-    mkdir -p "$TP_INSTALL_DIR/include/hs"
-    ''${BUILD_SYSTEM} install
-}
+    #mariadb-connector-c' '    ''${BUILD_SYSTEM} -j$PARALLEL
+        rm -rf "$TP_INSTALL_DIR/include/hs"
+        mkdir -p "$TP_INSTALL_DIR/include/hs"
+        ''${BUILD_SYSTEM} install
+    }
 
-#mariadb-connector-c'
+    #mariadb-connector-c'
 
-    export STARROCKS_HOME=$PWD
-    export STARROCKS_GCC_HOME=${stdenv.cc}
-    export CUSTOM_CMAKE=${cmakeWithPolicy}
-    export JAVA_HOME=${jdk}
-    export PARALLEL=''${NIX_BUILD_CORES:-1}
-    export THIRD_PARTY_BUILD_WITH_AVX2=OFF
+        export STARROCKS_HOME=$PWD
+        export STARROCKS_GCC_HOME=${stdenv.cc}
+        export CUSTOM_CMAKE=${cmakeWithPolicy}
+        export JAVA_HOME=${jdk}
+        export PARALLEL=''${NIX_BUILD_CORES:-1}
+        export THIRD_PARTY_BUILD_WITH_AVX2=OFF
 
-    ./thirdparty/build-thirdparty.sh
+        ./thirdparty/build-thirdparty.sh
 
-    runHook postBuild
+        runHook postBuild
   '';
 
   installPhase = ''
